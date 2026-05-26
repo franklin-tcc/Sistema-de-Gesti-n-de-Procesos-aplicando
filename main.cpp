9d1a6ff71cb88c09dd3a0735f1ec4f267feb4e27
61215751 <61215751@continental.edu.pe>
2026-05-25 23:05:56 -0500

    Add NodoMemoria structure and Insertar_Proceso function

--- main.cpp
+++ main.cpp
@@ -10,3 +10,14 @@ struct NodoProceso {
     string estado;
     NodoProceso* siguente;
 };
+struct NodoMemoria {
+ int cantidad_bloques;
+ int id_procesos;
+ NodoMemoria* siguiente;
+};
+void Insertar_Proceso(Nodoproceso*& cabeza, int id; string nombre, int prioridad, int mem){
+ NodoProceso* nuevo = new NodoProceso();
+ nuevo-
+
+  
+

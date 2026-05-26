2e3aa7ce732451e307d1446651c42e3c625c97fa
franklin-tcc <75178119@continental.edu.pe>
2026-05-25 22:50:41 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
-main.cpp
-lkdfmdfldvdmñlmdvmñldv
-jose
+#include <iostream>
+#include <string>
+ using namespace std;
+
+struct NodoProceso { 
+    int id; 
+    string nombre;
+    int prioridad;
+    int memoria;
+    string estado;
+    NodoProceso* siguente;
+};
